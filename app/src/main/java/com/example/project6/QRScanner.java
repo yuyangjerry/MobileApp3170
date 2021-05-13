3e7ef28f76c4ae6a36d53b9d2a4152dce293139b
Christian <cmar0027@student.monash.edu>
2021-05-13 18:48:47 +1000

    Added QRcode scanning functionality

---
+++ app/src/main/java/com/example/project6/QRScanner.java
@@ -0,0 +1,53 @@
+package com.example.project6;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.zxing.Result;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class QRScanner extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView mScannerView;
+    public static final String RESPONSE_INTENT_URL_KEY = "QRCODE";
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Register ourselves as a handler for scan results.
+        mScannerView.setResultHandler(this);
+        // Start camera on resume
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop camera on pause
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Intent intent = new Intent();
+        intent.putExtra(RESPONSE_INTENT_URL_KEY, rawResult.getText());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/project6/UserProfile.java
+++ app/src/main/java/com/example/project6/UserProfile.java
@@ -1,9 +1,17 @@
 package com.example.project6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -11,6 +19,13 @@ public class UserProfile extends AppCompatActivity {
     TextView email;
     TextView lastName;
     TextView uuid;
+
+    Button linkDoctor;
+
+    public final static int QR_CODE_REQUEST_CODE = 1;
+    public final static int CAMERA_PERMISSION_REQUEST_CODE = 2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,8 @@ public class UserProfile extends AppCompatActivity {
         email = findViewById(R.id.email);
         uuid = findViewById(R.id.uuid);
 
+        linkDoctor = findViewById(R.id.link_doctor_button);
+
         //get the auth client
         AuthClient auth = FakeAuthClient.getClient();
 
@@ -34,5 +51,54 @@ public class UserProfile extends AppCompatActivity {
         lastName.setText(user.getLastName());
         email.setText(user.getEmail());
         uuid.setText(user.getUuid().toString());
+
+
+        //When patient wants to link to doctor
+        linkDoctor.setOnClickListener(l -> {
+            // if patient has not given camera permission yet
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                //ask for camera permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+            }else{
+                //If we already have permission, just lounch the QRscanner activity which will take care of scanning
+                Intent intent = new Intent(this, QRScanner.class);
+                startActivityForResult(intent, QR_CODE_REQUEST_CODE);
+            }
+        });
+    }
+
+    // this will be triggered when the user responds to camera permission dialog
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            //if the permission was granted, just launch the QRscanner
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(this, QRScanner.class);
+                startActivityForResult(intent, QR_CODE_REQUEST_CODE);
+            } else {  //Otherwise show an error message
+                Toast.makeText(this, "You need to provide permission to use your camera in order to scan QRcode.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    //This will be triggered once the QRscanner has scanned something
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent response) {
+        if(requestCode == QR_CODE_REQUEST_CODE) {
+            //if the user scanned a qr code
+            if(resultCode == RESULT_OK){
+                //show the url
+                String url = response.getStringExtra(QRScanner.RESPONSE_INTENT_URL_KEY);
+                Toast.makeText(this, "URL: " + url, Toast.LENGTH_LONG).show();
+            }else{
+                //otherwise show an error message
+                Toast.makeText(this, "Could not scan QRcode.", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, response);
+        }
     }
 }
