e1e558b30c2880120f5bc8bc46c57370db0bafd2
Christian <cmar0027@student.monash.edu>
2021-08-22 19:41:11 +1000

    Moved QRcode scanner button to doctors fragment

--- app/src/main/java/com/FIT3170/HealthMonitor/DoctorsFragment.java
+++ app/src/main/java/com/FIT3170/HealthMonitor/DoctorsFragment.java
@@ -1,20 +1,38 @@
 package com.FIT3170.HealthMonitor;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class DoctorsFragment extends Fragment {
 
-    Button linkDoctorButton;
+    public final static int QR_CODE_REQUEST_CODE = 1;
+    public final static int CAMERA_PERMISSION_REQUEST_CODE = 2;
+
+    private Button linkDoctorButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_doctors, container, false);
+    }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -24,19 +42,55 @@ public class DoctorsFragment extends Fragment {
         // 1. for every doctor
         //      - add a fragment that links to the doctor profile
 
-        linkDoctorButton = view.findViewById(R.id.link_doctor_button);
+        linkDoctorButton = view.findViewById(R.id.link_new_doctor_button);
 
-//        linkDoctorButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //TODO: open the qr code scanner
-//            }
-//        });
+        linkDoctorButton.setOnClickListener(l -> {
+            Context context = getContext();
+            // if patient has not given camera permission yet
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                //ask for camera permission
+                Activity activity = getActivity();
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+            }else{
+                //If we already have permission, just launch the QRscanner activity which will take care of scanning
+                Intent intent = new Intent(context, QRScanner.class);
+                startActivityForResult(intent, QR_CODE_REQUEST_CODE);
+            }
+        });
     }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctors, container, false);
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == QR_CODE_REQUEST_CODE) {
+            Context context = getContext();
+            //if the user scanned a qr code
+            if(resultCode == Activity.RESULT_OK){
+                //show the url
+                String url = data.getStringExtra(QRScanner.RESPONSE_INTENT_URL_KEY);
+                Toast.makeText(context, "URL: " + url, Toast.LENGTH_LONG).show();
+            }else{
+                //otherwise show an error message
+                Toast.makeText(context, "Could not scan QRcode.", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            //if the permission was granted, just launch the QRscanner
+            Context context = getContext();
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(context, QRScanner.class);
+                startActivityForResult(intent, QR_CODE_REQUEST_CODE);
+            } else {  //Otherwise show an error message
+                Toast.makeText(context, "You need to provide permission to use your camera in order to scan QRcode.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/FIT3170/HealthMonitor/UserProfile.java
+++ app/src/main/java/com/FIT3170/HealthMonitor/UserProfile.java
@@ -24,7 +24,6 @@ public class UserProfile extends AppCompatActivity {
     TextView email;
     TextView uuid;
 
-    Button linkDoctor;
     Button notificationButton;
 
     public final static int QR_CODE_REQUEST_CODE = 1;
@@ -39,7 +38,6 @@ public class UserProfile extends AppCompatActivity {
         email = findViewById(R.id.email);
         uuid = findViewById(R.id.uuid);
 
-        linkDoctor = findViewById(R.id.link_doctor_button);
         notificationButton = findViewById(R.id.notificationButton);
 
         //get the currently signed in user
@@ -49,60 +47,11 @@ public class UserProfile extends AppCompatActivity {
         email.setText(user.getEmail());
         uuid.setText(user.getUid());
 
-
-        //When patient wants to link to doctor
-        linkDoctor.setOnClickListener(l -> {
-            // if patient has not given camera permission yet
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                //ask for camera permission
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
-            }else{
-                //If we already have permission, just lounch the QRscanner activity which will take care of scanning
-                Intent intent = new Intent(this, QRScanner.class);
-                startActivityForResult(intent, QR_CODE_REQUEST_CODE);
-            }
-        });
-
         createNotificationChannel();
         NotificationBuilder builder = new NotificationBuilder();
         notificationButton.setOnClickListener(l -> {
             builder.createNotification(this, "Abnormal Heart Rate", "An abnormal heart rate was detected. We recommend you get proper medical assistance.");
         });
-    }
-
-    // this will be triggered when the user responds to camera permission dialog
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            //if the permission was granted, just launch the QRscanner
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Intent intent = new Intent(this, QRScanner.class);
-                startActivityForResult(intent, QR_CODE_REQUEST_CODE);
-            } else {  //Otherwise show an error message
-                Toast.makeText(this, "You need to provide permission to use your camera in order to scan QRcode.", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    //This will be triggered once the QRscanner has scanned something
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent response) {
-        if(requestCode == QR_CODE_REQUEST_CODE) {
-            //if the user scanned a qr code
-            if(resultCode == RESULT_OK){
-                //show the url
-                String url = response.getStringExtra(QRScanner.RESPONSE_INTENT_URL_KEY);
-                Toast.makeText(this, "URL: " + url, Toast.LENGTH_LONG).show();
-            }else{
-                //otherwise show an error message
-                Toast.makeText(this, "Could not scan QRcode.", Toast.LENGTH_LONG).show();
-            }
-        }
-        else {
-            super.onActivityResult(requestCode, resultCode, response);
-        }
     }
 
     private void createNotificationChannel() {
