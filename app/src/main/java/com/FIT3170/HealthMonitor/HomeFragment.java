14e0c897a64ad3cda56c5f4ca242696274aff42a
Lasith Koswatta Gamage <lasithvindu1@gmail.com>
2021-08-27 12:50:31 +1000

    Connecting To Device

--- app/src/main/java/com/FIT3170/HealthMonitor/HomeFragment.java
+++ app/src/main/java/com/FIT3170/HealthMonitor/HomeFragment.java
@@ -1,57 +1,76 @@
 package com.FIT3170.HealthMonitor;
 
 import android.Manifest;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.ComponentName;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.fragment.NavHostFragment;
 
-import android.os.IBinder;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.FIT3170.HealthMonitor.adapters.ListAdapter_BleDevices;
 import com.FIT3170.HealthMonitor.bluetooth.BluetoothService;
 import com.FIT3170.HealthMonitor.bluetooth.BluetoothServiceViewModel;
 import com.FIT3170.HealthMonitor.database.UserProfile;
 import com.clj.fastble.data.BleDevice;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.auth.User;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
     private TextView email;
     private TextView uuid;
 
     // Bluetooth
-    private BluetoothService service;
-    private BleDevice device;
+    private BluetoothService mService;
+    private BleDevice mDevice;
+    private Boolean mIsScanning;
     private BluetoothServiceViewModel model;
+    public static final int REQUEST_FINE_LOCATION = 2;
+
+    // UI
+    private ListAdapter_BleDevices adapter;
+    private ListView scannedDevicesListView;
+    private ProgressDialog progressDialog;
+    private Button scanDevicesBtn;
+        // Button States
+        private final int BTN_SCANNING = 1;
+        private final int BTN_DISABLED = 2;
+        private final int BTN_ENABLED = 3;
+
+
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        // Set Observers
+        setObservers();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    private void initView(View view) {
         //get all needed views by id
         email = view.findViewById(R.id.email);
         uuid = view.findViewById(R.id.uuid);
@@ -62,15 +81,30 @@ public class HomeFragment extends Fragment {
         email.setText(UserProfile.getEmail());
         uuid.setText(UserProfile.getUid());
 
-//        // Set Observers
-        setObservers();
+        // Adapter
+        adapter = new ListAdapter_BleDevices(getActivity());
+        adapter.setOnDeviceClickListener(new ListAdapter_BleDevices.OnDeviceClickListener() {
+            @Override
+            public void onAction(BleDevice bleDevice) {
+                if (adapter.getButtonState() == ListAdapter_BleDevices.BTN_CONNECTED) {
+                    disconnectAllDevices();
+                } else if (adapter.getButtonState() == ListAdapter_BleDevices.BTN_DISCONNECTED) {
+                    connectDevice(bleDevice);
+                }
+            }
+        });
 
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // ListView
+        scannedDevicesListView = (ListView) view.findViewById(R.id.homeFrgLstViewScanDevices);
+        scannedDevicesListView.setAdapter(adapter);
+
+        // Scan Button
+        scanDevicesBtn = (Button) view.findViewById(R.id.homeFrgBtnScanDevices);
+        scanDevicesBtn.setOnClickListener(this);
+        setScanButtonState(BTN_DISABLED);
+
+        // Progress
+        progressDialog = new ProgressDialog(getActivity());
     }
 
     private void setObservers() {
@@ -82,11 +116,20 @@ public class HomeFragment extends Fragment {
                 }
                 else{
                     Log.d("debug", "onChanged: bound to service.");
-                    service = bluetoothBinder.getService();
+                    mService = bluetoothBinder.getService();
 
                     // Device Observer
                     // BleDevice
-                    service.getBleDevice().observe(getActivity(), bleDeviceObserver);
+                    mDevice = mService.getmBleDevice().getValue();
+                    mService.getmBleDevice().observe(getActivity(), bleDeviceObserver);
+                    // isScanning
+                    mIsScanning = mService.isScanning().getValue();
+                    mService.isScanning().observe(getActivity(),isScanningObserver);
+                    if (!mIsScanning) {
+                        setScanButtonState(BTN_ENABLED);
+                    }
+                    // Device Scanner Observer
+                    mService.getmScanResult().observe(getActivity(),scanResultObserver);
                 }
             }
         });
@@ -94,6 +137,7 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onResume() {
+        Log.d("debug", "HomeFragment: onResume");
         super.onResume();
         startService();
     }
@@ -101,6 +145,10 @@ public class HomeFragment extends Fragment {
     @Override
     public void onStop() {
         super.onStop();
+        Log.d("debug", "HomeFragment: onStop");
+//        if (mDevice != null) {
+//            disconnectAllDevices();
+//        }
         if(model.getBinder() != null){
             getActivity().unbindService(model.getServiceConnection());
         }
@@ -117,15 +165,130 @@ public class HomeFragment extends Fragment {
         getActivity().bindService(serviceIntent, model.getServiceConnection(), Context.BIND_AUTO_CREATE);
     }
 
+    private void setScanButtonState(int buttonState) {
+        switch (buttonState) {
+            case BTN_ENABLED: {
+                scanDevicesBtn.setText("Scan Devices");
+                scanDevicesBtn.setEnabled(true);
+                break;
+            }
+            case BTN_DISABLED: {
+                scanDevicesBtn.setEnabled(false);
+                break;
+            }
+            case BTN_SCANNING: {
+                scanDevicesBtn.setText("Scanning...");
+                scanDevicesBtn.setEnabled(false);
+                break;
+            }
+        }
+    }
+
     Observer<BleDevice> bleDeviceObserver = new Observer<BleDevice>() {
         @Override
         public void onChanged(BleDevice bleDevice) {
-            device = bleDevice;
+            // When device connects
+            if (bleDevice != null) {
+                adapter.addDevice(bleDevice);
+                adapter.notifyDataSetChanged();
+                Log.d("debug","successfully received BleDevice on HomeFragment");
+            } else {
+                // When device disconnects
+                if (mDevice != null) {
+                    // Replace the connected device, with the disconnected device!
+                    adapter.replaceDevice(mDevice);
+                    adapter.notifyDataSetChanged();
+                }
+                Log.d("debug","disconnected BleDevice on HomeFragment");
+            }
+            // Update UI about changes to device
+            // ...
+//            mDevice = bleDevice;
+        }
+    };
+
+    Observer<Boolean> isScanningObserver = new Observer<Boolean>() {
+        @Override
+        public void onChanged(Boolean isScanning) {
+            mIsScanning =  isScanning;
+            // Update UI about changes to device
+            // ...
+            Log.d("debug", "isScanning: "+isScanning.toString());
+            if (mIsScanning) {
+                setScanButtonState(BTN_SCANNING);
+            } else {
+                setScanButtonState(BTN_ENABLED);
+            }
+        }
+    };
+
+    Observer<BleDevice> scanResultObserver = new Observer<BleDevice>() {
+        @Override
+        public void onChanged(BleDevice device) {
+            Log.d("debug","scan");
+            if (device != null) {
+                adapter.addDevice(device);
+                adapter.notifyDataSetChanged();
+            }
             // Update UI about changes to device
             // ...
         }
     };
 
+    @Override
+    public void onClick(View view) {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)  != PackageManager.PERMISSION_GRANTED) {
+            // Should an explanation to users, why we require their fine location services
+            new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.location_permissions_title)
+                    .setMessage(R.string.location_permissions_text)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            //Prompt the user once explanation has been shown
+                            ActivityCompat.requestPermissions(getActivity(),
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    REQUEST_FINE_LOCATION);
+                            // Calls the onRequestPermissionsResult() function once user enters an input
+                            // This function will call the startScan() function
+                        }
+                    })
+                    .create()
+                    .show();
+        } else {
+            startScan();
+        }
+    }
 
+    private void startScan() {
+        if (!mIsScanning) {
+            if (mService != null) {
+                adapter.clearScanDevice();
+                adapter.notifyDataSetChanged();
+                mService.startScan();
+            }
+        }
+    }
 
+    private void connectDevice(BleDevice device) {
+        stopScan();
+        if (device != null) {
+            if (mService != null) {
+                Log.d("debug", "connected to device");
+                mService.connectDevice(device);
+            }
+        }
+    }
+
+    private void disconnectAllDevices() {
+        if (mService != null) {
+            mService.disconnectAllDevices();
+        }
+    }
+
+    private void stopScan() {
+        if (mService != null) {
+            mService.stopScan();
+        }
+    }
 }
--- app/src/main/java/com/FIT3170/HealthMonitor/bluetooth/BluetoothService.java
+++ app/src/main/java/com/FIT3170/HealthMonitor/bluetooth/BluetoothService.java
@@ -2,26 +2,38 @@ package com.FIT3170.HealthMonitor.bluetooth;
 
 
 import android.app.Service;
+import android.bluetooth.BluetoothGatt;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.FIT3170.HealthMonitor.MainActivity;
 import com.clj.fastble.BleManager;
+import com.clj.fastble.callback.BleGattCallback;
+import com.clj.fastble.callback.BleScanCallback;
 import com.clj.fastble.data.BleDevice;
+import com.clj.fastble.exception.BleException;
 import com.clj.fastble.scan.BleScanRuleConfig;
 
+import java.util.List;
 import java.util.UUID;
 
 public class BluetoothService extends Service {
     private final IBinder binder = new BluetoothBinder();
-    private MutableLiveData<Boolean> isScanning = new MutableLiveData<>(false);
-    private MutableLiveData<Boolean> isPermitted = new MutableLiveData<>(false);
-    private MutableLiveData<BleDevice> bleDevice = new MutableLiveData<>();
+    private MutableLiveData<Boolean> mIsScanning = new MutableLiveData<>(false);
+    private MutableLiveData<BleDevice> mBleDevice = new MutableLiveData<>();
+    private MutableLiveData<BleDevice> mScanResult = new MutableLiveData<>();
+
+    // Connection States
+    private MutableLiveData<Integer> connectionStatus = new MutableLiveData<>();
+    public static final int CONNECTION_INITIATE = 1;
+    public static final int CONNECTION_SUCCESS = 2;
+    public static final int CONNECTION_FAILURE = 3;
+    public static final int CONNECTION_DISCONNECTED = 4;
 
     @Nullable
     @Override
@@ -71,21 +83,91 @@ public class BluetoothService extends Service {
     }
 
     public MutableLiveData<Boolean> isScanning() {
-        return isScanning;
+        return mIsScanning;
     }
 
-    public MutableLiveData<Boolean> isPermitted() {
-        return isPermitted;
-    }
 
-    public MutableLiveData<BleDevice> getBleDevice() {return bleDevice; }
+    public MutableLiveData<BleDevice> getmBleDevice() {return mBleDevice; }
 
-    public void setBleDevice(BleDevice device) {bleDevice.postValue(device);}
+    public MutableLiveData<BleDevice> getmScanResult() {return mScanResult; }
+
+    public void setmBleDevice(BleDevice device) {
+        mBleDevice.postValue(device);}
 
     @Override
     // Stops the service
     public void onTaskRemoved(Intent rootIntent) {
         super.onTaskRemoved(rootIntent);
+        BleManager.getInstance().disconnectAllDevice();
         stopSelf();
     }
+
+    public void startScan() {
+        if (!mIsScanning.getValue()) {
+            BleManager.getInstance().scan(new BleScanCallback() {
+                @Override
+                public void onScanStarted(boolean success) {
+                    mIsScanning.postValue(true);
+//                img_loading.startAnimation(operatingAnim);
+//                img_loading.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onLeScan(BleDevice bleDevice) {
+                    super.onLeScan(bleDevice);
+                }
+
+                @Override
+                public void onScanning(BleDevice bleDevice) {
+                    mScanResult.postValue(bleDevice);
+                }
+
+                @Override
+                public void onScanFinished(List<BleDevice> scanResultList) {
+                    mIsScanning.postValue(false);
+                }
+            });
+        }
+    }
+
+    public void stopScan() {
+        if (mIsScanning.getValue()) {
+            BleManager.getInstance().cancelScan();
+            mIsScanning.postValue(false);
+        }
+    }
+
+    public void connectDevice(BleDevice device) {
+        if (device != null) {
+            BleManager.getInstance().connect(device, new BleGattCallback() {
+                @Override
+                public void onStartConnect() {
+                    connectionStatus.postValue(CONNECTION_INITIATE);
+                }
+
+                @Override
+                public void onConnectFail(BleDevice bleDevice, BleException exception) {
+                    connectionStatus.postValue(CONNECTION_FAILURE);
+                }
+
+                @Override
+                public void onConnectSuccess(BleDevice bleDevice, BluetoothGatt gatt, int status) {
+                    connectionStatus.postValue(CONNECTION_SUCCESS);
+                    mBleDevice.postValue(bleDevice);
+                }
+
+                @Override
+                public void onDisConnected(boolean isActiveDisConnected, BleDevice bleDevice, BluetoothGatt gatt, int status) {
+                    connectionStatus.postValue(CONNECTION_DISCONNECTED);
+                    mBleDevice.postValue(null);
+                }
+            });
+        }
+
+    }
+
+    public void disconnectAllDevices() {
+        BleManager.getInstance().disconnectAllDevice();
+    }
+
 }
